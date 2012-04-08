dd2990e2732e1d72c0c5ca329682ce834f85715d
Max Wang <mxawng@gmail.com>
2012-04-08 19:09:36 -0400

    Client socket channel creation

--- libmotmot/main.c
+++ libmotmot/main.c
@@ -1,43 +1,59 @@
 #include <assert.h>
 #include <errno.h>
-#include <glib.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
 
-#define err(cond, errstr) \
-  if (cond) { \
-    perror(errstr); \
-    exit(1); \
+#include <glib.h>
+
+#define err(cond, errstr)   \
+  if (cond) {               \
+    perror(errstr);         \
+    exit(1);                \
   }
 
 GMainLoop *gmain;
 
+/**
+ * Create a local UNIX socket and wrap it in a GIOChannel.
+ *
+ * All our socket files are identified by conn/xxxxxx, where xxxxxx is the
+ * pid of the listening process.  Passing pid > 0 to create_socket will
+ * open a connection to the corresponding file; otherwise, a listening
+ * socket file will be created with the pid of the calling process.
+ */
 GIOChannel *
-open_listening_socket()
+create_socket_channel(int pid)
 {
   int s;
   char *filename;
-  struct sockaddr_un *local;
+  struct sockaddr_un *saddr;
   GIOChannel *channel;
 
-  // 14 = sizeof(short) + "conn/000000"
-  local = g_malloc0(14);
-  local->sun_family = AF_UNIX;
+  // 14 = sizeof(short) + "conn/000000".
+  saddr = g_malloc0(14);
+  saddr->sun_family = AF_UNIX;
 
-  // "Six digits should be enough for a pid"
-  sprintf((char *)local + sizeof(short), "conn/%06d", getpid());
+  // "Six digits should be enough for a pid."
+  sprintf(saddr->sun_path, "conn/%06d", (pid > 0) ? pid : getpid());
 
-  // Go through the socket opening song and dance
-  err((s = socket(PF_LOCAL, SOCK_STREAM, 0)) == -1, "Unable to open socket");
-  err(bind(s, (struct sockaddr *)local, 14), "Unable to bind socket");
-  err(listen(s, 5) == -1, "Unable to listen on socket");
+  // Open a socket.
+  s = socket(PF_LOCAL, SOCK_STREAM, 0);
+  err(s < 0, "socket");
 
+  if (pid > 0) {
+    // Connect to conn/pid.
+    err(connect(s, (struct sockaddr *)saddr, 14) < 0, "connect");
+  } else {
+    // Listen on conn/pid.
+    err(bind(s, (struct sockaddr *)saddr, 14) < 0, "bind");
+    err(listen(s, 5) < 0, "listen");
+  }
   channel = g_io_channel_unix_new(s);
 
-  g_free(local);
+  g_free(saddr);
   return channel;
 }
 
@@ -51,21 +67,22 @@ int
 socket_accept(GIOChannel *source, GIOCondition condition, void *data)
 {
   int fd, newfd, len;
-  struct sockaddr_un *socket;
+  struct sockaddr_un *saddr;
   GIOChannel *channel;
-
-  socket = g_new0(struct sockaddr_un, 1);
 
   fd = g_io_channel_unix_get_fd(source);
 
-  newfd = accept(fd, (struct sockaddr *)socket, &len);
-  err(newfd == -1, "Error accepting a new connection");
+  saddr = g_new0(struct sockaddr_un, 1);
+
+  newfd = accept(fd, (struct sockaddr *)saddr, &len);
+  err(newfd < 0, "accept");
 
   channel = g_io_channel_unix_new(newfd);
 
   // TODO: pass in some data here
   g_io_add_watch(channel, G_IO_IN, socket_recv, NULL);
 
+  g_free(saddr);
   return TRUE;
 }
 
@@ -76,7 +93,7 @@ main(int argc, char *argv[])
 
   gmain = g_main_loop_new(g_main_context_default(), FALSE);
 
-  channel = open_listening_socket();
+  channel = create_socket_channel(0);
 
   g_io_add_watch(channel, G_IO_IN, socket_accept, NULL);
 
