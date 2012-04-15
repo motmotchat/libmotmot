9daf848432989eabb454a5d724e13cc258f53b69
Carl Jackson <carl@avtok.com>
2012-04-15 02:28:26 -0400

    Better testing library

--- libmotmot/test/basic.rb
+++ libmotmot/test/basic.rb
@@ -6,12 +6,12 @@ spawn 5 do |clients|
   clients.each_with_index do |client, i|
     client.write "Hello world! %d\n" % i
   end
-  loop do
-    rs, = IO.select clients, [], [], 1
-    break if rs.nil?
-
-    for r in rs
-      puts "PID %06d said: %s" % [r.pid, r.gets]
-    end
+  clients.pump
+  #clients.kill 2
+  clients.spawn 4
+  sleep 2
+  clients.each_with_index do |client, i|
+    client.write "More people %d\n" % i
   end
+  clients.pump
 end
--- libmotmot/test/lib.rb
+++ libmotmot/test/lib.rb
@@ -7,24 +7,54 @@ ROOT = File.dirname(__FILE__) + '/../'
 MOTMOT_PATH = ROOT + 'src/motmot'
 CONN_PATH = 'conn/'
 
+class MotMotPool < Array
+  def spawn n = 1
+    n.times do
+      self << IO.popen([MOTMOT_PATH], 'w+')
+    end
+  end
+
+  def kill n = 1
+    # TODO(carl): fix this
+    self.sort_by!{rand}.pop(n).each do |p|
+      Process.kill 'TERM', p.pid
+      p.close
+    end
+  end
+
+  def kill_all
+    self.each do |p|
+      Process.kill 'TERM', p.pid
+      p.close
+    end
+    self.replace []
+  end
+
+  def pump timeout=1
+    loop do
+      rs, = IO.select self, [], [], timeout
+      return if rs.nil?
+
+      for r in rs
+        puts "PID %06d said: %s" % [r.pid, r.gets]
+      end
+    end
+  end
+end
+
 module Kernel
   def spawn n
     FileUtils.mkdir_p CONN_PATH
-    clients = (1..n).map do
-      IO.popen [MOTMOT_PATH], 'w+'
-    end
-    # Sleep a bit until all the clients pick each other up
-    sleep 1.5
+
+    clients = MotMotPool.new
+
     begin
-      puts "=" * 80
+      clients.spawn n
+      sleep 2 # Wait for initial processes to pick each other up
       yield clients
-      puts "=" * 80
     ensure
       # Burn them ALL with FIRE
-      clients.map do |p|
-        Process.kill 'TERM', p.pid
-        p.close
-      end
+      clients.kill_all
     end
   end
 end
