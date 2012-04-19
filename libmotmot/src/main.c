cfb568658ae799e18674da5664e9f4de4836fb52
Carl Jackson <carl@avtok.com>
2012-04-19 05:31:09 -0400

    Rewrite main thingy

--- libmotmot/src/main.c
+++ libmotmot/src/main.c
@@ -9,13 +9,9 @@
 #include <unistd.h>
 
 #include <glib.h>
-#include <glib-unix.h>
 #include <msgpack.h>
 
 #include "motmot.h"
-
-#define SOCKDIR   "conn/"
-#define MAXCONNS  100
 
 #define err(cond, errstr)                           \
   if (cond) {                                       \
@@ -25,88 +21,57 @@
 
 GMainLoop *gmain;
 
-int conns[MAXCONNS];
-
 /**
  * Create a local UNIX socket and wrap it in a GIOChannel.
- *
- * All our socket files are identified by conn/xxxxxx, where xxxxxx is the
- * pid of the listening process.  Passing pid > 0 to create_socket will
- * open a connection to the corresponding file; otherwise, a listening
- * socket file will be created with the pid of the calling process.
  */
 GIOChannel *
-create_socket_channel(int pid)
+open_socket(const char *handle, size_t len, bool listening)
 {
   int s;
   struct sockaddr_un *saddr;
   GIOChannel *channel;
-  GError *gerr;
 
-  // 14 = sizeof(short) + "conn/000000".
-  saddr = g_malloc0(14);
+  saddr = g_malloc0(sizeof(short) + len);
   saddr->sun_family = AF_UNIX;
+  memcpy(&saddr->sun_path, handle, len);
 
-  // "Six digits should be enough for a pid."
-  sprintf(saddr->sun_path, SOCKDIR "%06d", (pid > 0) ? pid : getpid());
+  err((s = socket(PF_LOCAL, SOCK_STREAM, 0)) < 0, "socket");
 
-  // Open a socket.
-  s = socket(PF_LOCAL, SOCK_STREAM, 0);
-  err(s < 0, "socket");
-
-  if (pid > 0) {
-    // Connect to conn/pid.
-    if (connect(s, (struct sockaddr *)saddr, 14) < 0) {
+  if (listening) {
+    err(bind(s, (struct sockaddr *)saddr, sizeof(short) + len) < 0, "bind");
+    err(listen(s, 5) < 0, "listen");
+  } else {
+    if (connect(s, (struct sockaddr *)saddr, sizeof(short) + len) < 0) {
       // Connection refused. Probably a stale socket, so just ignore it
+      g_warning("Connection refused");
       return NULL;
     }
-  } else {
-    // Listen on conn/pid.
-    err(bind(s, (struct sockaddr *)saddr, 14) < 0, "bind");
-    err(listen(s, 5) < 0, "listen");
-  }
-
-  gerr = NULL;
-  channel = g_io_channel_unix_new(s);
-  if (g_io_channel_set_encoding(channel, NULL, &gerr) == G_IO_STATUS_ERROR) {
-    // TODO: error handling
-    g_error("create_socket_channel: Failed to set channel encoding.\n");
   }
 
   g_free(saddr);
+
+  channel = g_io_channel_unix_new(s);
+  if (g_io_channel_set_encoding(channel, NULL, NULL) == G_IO_STATUS_ERROR) {
+    g_error("socket_to_gio: Failed to set channel encoding.\n");
+    return NULL;
+  }
+
   return channel;
+}
+
+GIOChannel *
+listen_unix(const char *handle, size_t len)
+{
+  return open_socket(handle, len, true);
 }
 
 /**
  * Wrapper around create_socket_channel.
  */
 GIOChannel *
-create_socket_channel_motmot(const char *msg, size_t len)
+connect_unix(const char *handle, size_t len)
 {
-  int pid = atoi(msg);
-  if (pid != 0) {
-    return create_socket_channel(pid);
-  } else {
-    return NULL;
-  }
-}
-
-/**
- * Clean up the mess we have made.  Currently, this just involves removing our
- * listening socket in SOCKDIR.
- */
-int
-cleanup(void *data)
-{
-  // Something something avoid mallocing near signal handlers something.
-  static char sockname[12];
-
-  sprintf(sockname, SOCKDIR "%06d", getpid());
-  unlink(sockname);
-
-  // Let's get out of here.
-  g_main_loop_quit(gmain);
-  return FALSE;
+  return open_socket(handle, len, false);
 }
 
 int
@@ -150,44 +115,6 @@ socket_accept(GIOChannel *source, GIOCondition condition, void *data)
 }
 
 int
-poll_conns(void *data)
-{
-  int i, pid;
-  bool found;
-  DIR *dp;
-  struct dirent *ep;
-
-  // Open up conn/.
-  dp = opendir(SOCKDIR);
-  err(dp == NULL, "opendir");
-
-  // Search through all connections.
-  while ((ep = readdir(dp))) {
-    // Ignore . and .. and any files that shouldn't exist.
-    pid = atoi(ep->d_name);
-    if (pid == 0 || pid == getpid()) {
-      continue;
-    }
-
-    // Check whether we have already connected to the socket.
-    found = false;
-    for (i = 0; conns[i] != 0; ++i) {
-      if (pid == conns[i]) {
-        found = true;
-        break;
-      }
-    }
-
-    if (!found) {
-      // TODO: Connect.
-    }
-  }
-
-  closedir(dp);
-  return TRUE;
-}
-
-int
 input_loop(GIOChannel *channel, GIOCondition condition, void *data)
 {
   char *msg;
@@ -215,27 +142,31 @@ input_loop(GIOChannel *channel, GIOCondition condition, void *data)
 int
 main(int argc, char *argv[])
 {
+  int i;
   GIOChannel *channel;
+
+  if (argc < 2) {
+    printf("Usage: motmot my/sock [other/socks...]\n");
+    exit(1);
+  }
 
   // Set up the main event loop.
   gmain = g_main_loop_new(g_main_context_default(), FALSE);
 
   // Create our server / listening socket.
-  channel = create_socket_channel(0);
+  channel = listen_unix(argv[1], strlen(argv[1]));
   g_io_add_watch(channel, G_IO_IN, socket_accept, NULL);
 
-  // Let's try to clean up ourselves in the case someone kills us.
-  g_unix_signal_add(SIGINT, cleanup, NULL);
-  g_unix_signal_add(SIGTERM, cleanup, NULL);
-
-  // Poll the directory for new sockets every second.
-  g_timeout_add_seconds(1, poll_conns, NULL);
-
-  // Add the input loop as an idle source.
+  // Watch for input on stdin
   g_io_add_watch(g_io_channel_unix_new(0), G_IO_IN, input_loop, NULL);
 
   // Initialize motmot.
-  motmot_init(create_socket_channel_motmot, NULL, NULL, NULL);
+  motmot_init(connect_unix, NULL, NULL, NULL);
+
+  for (i = 2; i < argc; i++) {
+    channel = connect_unix(argv[i], strlen(argv[i]));
+    // TODO: add paxos participant
+  }
 
   g_main_loop_run(gmain);
 
