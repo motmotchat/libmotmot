458fae6b5e15b9eaccf6048fbf78b2f10940e728
Max Wang <mxawng@gmail.com>
2012-04-22 03:41:06 -0400

    Newlines in paxop_print

--- libmotmot/src/paxos_print.c
+++ libmotmot/src/paxos_print.c
@@ -11,49 +11,49 @@ paxop_print(paxop_t op)
 {
   switch (op) {
     case OP_PREPARE:
-      printf("OP_PREPARE");
+      printf("OP_PREPARE\n");
       break;
     case OP_PROMISE:
-      printf("OP_PROMISE");
+      printf("OP_PROMISE\n");
       break;
     case OP_DECREE:
-      printf("OP_DECREE");
+      printf("OP_DECREE\n");
       break;
     case OP_ACCEPT:
-      printf("OP_ACCEPT");
+      printf("OP_ACCEPT\n");
       break;
     case OP_COMMIT:
-      printf("OP_COMMIT");
+      printf("OP_COMMIT\n");
       break;
     case OP_WELCOME:
-      printf("OP_WELCOME");
+      printf("OP_WELCOME\n");
       break;
     case OP_GREET:
-      printf("OP_GREET");
+      printf("OP_GREET\n");
       break;
     case OP_HELLO:
-      printf("OP_HELLO");
+      printf("OP_HELLO\n");
       break;
     case OP_PTMY:
-      printf("OP_PTMY");
+      printf("OP_PTMY\n");
       break;
     case OP_REQUEST:
-      printf("OP_REQUEST");
+      printf("OP_REQUEST\n");
       break;
     case OP_RETRIEVE:
-      printf("OP_RETRIEVE");
+      printf("OP_RETRIEVE\n");
       break;
     case OP_RESEND:
-      printf("OP_RESEND");
+      printf("OP_RESEND\n");
       break;
     case OP_REDIRECT:
-      printf("OP_REDIRECT");
+      printf("OP_REDIRECT\n");
       break;
     case OP_SYNC:
-      printf("OP_SYNC");
+      printf("OP_SYNC\n");
       break;
     case OP_TRUNCATE:
-      printf("OP_TRUNCATE");
+      printf("OP_TRUNCATE\n");
       break;
   }
 }
