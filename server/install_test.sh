1817905c8f79bcb867c18ac3500466947e52074f
EJ Bensing <ebensing@college.harvard.edu>
2012-05-04 20:32:54 +0000

    small modification to the testing install script

--- server/install_test.sh
+++ server/install_test.sh
@@ -5,3 +5,4 @@
 sqlite3 config.db "INSERT INTO users(userName, password) values ('ej@bensing.com','12345');"
 
 sqlite3 config.db "INSERT INTO users(userName, password) values ('ej@bensing2.com','12345');"
+sqlite3 config.db "INSERT INTO users(userName, password) values ('ej@bensing1.com','12345');"
