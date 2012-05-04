21e0196ce71b9b298a9a15d49a05b61a0b095b66
Julie Hansbrough <azureye@gmail.com>
2012-05-04 05:21:10 -0400

    carl thinks i'm a loser

--- plugin/motmot.h
+++ plugin/motmot.h
@@ -32,3 +32,18 @@ typedef struct {
   gpointer room;
   GHashTable *components;
 } MotmotInfo;
+
+/*
+  Seems as good a place as any to put this (for my benefit as much as anyone else's)
+
+  General overview of control flow for motmot functions:
+
+  To send a message, purplemot_chat_send calls motmot_send, which handles paxos.
+  Once paxos is handled, print_chat_motmot calls receive_chat_message.
+
+  To join a chat, purplemot_join_chat calls motmot_invite, which handles paxos.
+  Once paxos is handled, print_join_motmot tells everyone that someone joined.
+
+  To leave a chat, purplemot_chat_leave calls motmot_disconnect, which handles paxos.
+  Once paxos is handled, print_part_motmot calls left_chat_room to tell everyone who left.
+*/
