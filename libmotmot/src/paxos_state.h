3ef8025c65006170f3191fcca016c45144f6ac51
Max Wang <mxawng@gmail.com>
2012-07-01 01:48:28 -0700

    Whoops, forgot to add paxos_state.h

---
+++ libmotmot/src/paxos_state.h
@@ -0,0 +1,24 @@
+/**
+ * paxos_state.h - Global state of the multi-session Paxos protocol.
+ */
+
+#include "paxos.h"
+
+struct paxos_state {
+  connect_t connect;                  // callback for initiating connections
+  enter_t enter;                      // callback for entering chat
+  leave_t leave;                      // callback for leaving chat
+  struct learn_table learn;           // callbacks for paxos_learn
+
+  session_container sessions;         // list of active Paxos sessions
+  connect_container *connections;     // hash table of connections
+};
+
+extern struct paxos_state state;
+
+/**
+ * This variable always references the current session from the point of view
+ * of the current thread.  If we were multithreading, it would be thread-local,
+ * but of course we aren't.
+ */
+extern struct paxos_session *pax;
