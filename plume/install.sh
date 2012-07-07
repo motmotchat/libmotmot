c7fe207148f6802549bd150f28ef2e6dbf003627
Carl Jackson <carl@avtok.com>
2012-07-07 16:28:30 -0700

    Get rid of old code

--- plume/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#This script bootstraps a new sqlite db for testing
-sqlite3 config.db "CREATE TABLE users(userId INTEGER PRIMARY KEY AUTOINCREMENT, userName TEXT, password TEXT);"
-#sqlite3 config.db "INSERT INTO users(userName,password) values('jeff@motmottest.com','12345');"
-#sqlite3 config.db "INSERT INTO users(userName,password) values('jeff2@motmottest.com','12345');"
-sqlite3 config.db "CREATE TABLE friends(friendId INTEGER PRIMARY KEY AUTOINCREMENT, userName TEXT, friend TEXT, accepted BOOLEAN);"
-mkdir cert
-mkdir ctest
--- plume/install_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# insert test users for testing
-
-sqlite3 config.db "INSERT INTO users(userName, password) values ('ej@bensing.com','12345');"
-
-sqlite3 config.db "INSERT INTO users(userName, password) values ('ej@bensing2.com','12345');"
-sqlite3 config.db "INSERT INTO users(userName, password) values ('ej@bensing1.com','12345');"
