99a78b1de334c41e493862ec9cdf6bc61351bca9
Carl Jackson <carl@avtok.com>
2012-12-11 17:19:17 -0500

    Parse the other party's address off of stdin

--- src/trill.c
+++ src/trill.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <glib.h>
 
 #include "log.h"
@@ -52,6 +53,8 @@ main(int argc, char *argv[])
 {
   GIOChannel *chan;
   struct trill_connection *conn;
+  char buf[25];
+  char *buf_ptr;
 
   gmain = g_main_loop_new(g_main_context_default(), 0);
 
@@ -63,7 +66,18 @@ main(int argc, char *argv[])
   chan = g_io_channel_unix_new(conn->tc_sock_fd);
   conn->tc_event_loop_descriptor = chan;
 
-  trill_connection_connect(conn, "127.0.0.1", 9000);
+  // Saddest parser ever
+  fputs("Who do you want to connect to? ", stdout);
+  fflush(stdout);
+  fgets(buf, sizeof(buf), stdin);
+  buf_ptr = buf;
+  while (*buf_ptr != ':' && *buf_ptr != ' ' && *buf_ptr != '\0') {
+    buf_ptr++;
+  }
+  *buf_ptr++ = '\0';
+
+  trill_connection_connect(conn, buf, atoi(buf_ptr));
+  log_info("Connecting to %s on port %d", buf, atoi(buf_ptr));
 
   g_io_add_watch(chan, G_IO_IN, socket_can_read, conn);
 
