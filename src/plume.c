7b03672c860d692abede29ea2f4737b87457a703
Max Wang <mxawng@gmail.com>
2012-12-14 15:54:23 -0500

    More server connect and TLS support

--- src/plume.c
+++ src/plume.c
@@ -9,15 +9,59 @@
 
 GMainLoop *gmain;
 
-void
-plume_connect_server(GObject *resolver, GAsyncResult *res, void *data)
+int
+plume_tls_verify_cert(GTlsConnection *conn, GTlsCertificate *cert,
+    GTlsCertificateFlags errors, void *data)
 {
-  GList *srvs;
+  log_warn("plume_tls_verify_cert");
 
-  srvs = g_resolver_lookup_service_finish((GResolver *)resolver, res, NULL);
+  return 1;
+}
 
-  log_info("%s:%hu", g_srv_target_get_hostname(srvs->data),
-      g_srv_target_get_port(srvs->data));
+void
+plume_tls_setup(GSocketClient *client, GSocketClientEvent event,
+    GSocketConnectable *addr, GIOStream *conn, void *data)
+{
+  GTlsCertificate *cert;
+  GTlsClientConnection *tls_conn;
+
+  log_warn("plume_tls_setup: %d", event);
+
+  if (event != G_SOCKET_CLIENT_TLS_HANDSHAKING) {
+    return;
+  }
+
+  tls_conn = (GTlsClientConnection *)conn;
+
+  cert = g_tls_certificate_new_from_file("client.crt", NULL);
+  g_tls_connection_set_certificate((GTlsConnection *)tls_conn, cert);
+  g_tls_client_connection_set_validation_flags(tls_conn, 0);
+
+  g_signal_connect(tls_conn, "accept-certificate",
+      (GCallback)plume_tls_verify_cert, NULL);
+}
+
+void
+plume_connect_server(GObject *obj, GAsyncResult *res, void *data)
+{
+  GResolver *resolver;
+  GSrvTarget *srv;
+
+  GSocketConnectable *addr;
+  GSocketClient *client;
+  GSocketConnection *conn;
+
+  resolver = (GResolver *)obj;
+  srv = g_resolver_lookup_service_finish(resolver, res, NULL)->data;
+
+  client = g_socket_client_new();
+  g_socket_client_set_tls(client, TRUE);
+  addr = g_network_address_new(g_srv_target_get_hostname(srv),
+      g_srv_target_get_port(srv));
+
+  g_signal_connect(client, "event", (GCallback)plume_tls_setup, NULL);
+
+  conn = g_socket_client_connect(client, addr, NULL, NULL);
 }
 
 int
